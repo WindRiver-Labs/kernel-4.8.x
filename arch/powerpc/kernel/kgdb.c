bfdcd90b3204c98459270f97574cd14c9c5a44e0
Tiejun Chen <tiejun.chen@windriver.com>
2012-10-27 10:18:40 +0800

    powerpc/kgdb: Fix a single stgep case of lazy IRQ
    When we're in kgdb_singlestep(), we have to work around to get
    thread_info by copying from the kernel stack before calling
    kgdb_handle_exception(), then copying it back afterwards.
    
    But for PPC64, we have a lazy interrupt implementation. So after
    copying thread info frome kernle stack, if we need to replay an
    interrupt, we shouldn't restore that previous backup thread info
    to make sure we can replay an interrupt lately with a proper
    thread info.
    
    This patch use __check_irq_replay() to guarantee this process.
    
    Signed-off-by: Tiejun Chen <tiejun.chen@windriver.com>
    Signed-off-by: Bin Jiang <bin.jiang@windriver.com>

--- arch/powerpc/kernel/kgdb.c
+++ arch/powerpc/kernel/kgdb.c
@@ -150,6 +150,7 @@ static int kgdb_handle_breakpoint(struct pt_regs *regs)
 	return 1;
 }
 
+extern notrace unsigned int check_irq_replay(void);
 static DEFINE_PER_CPU(struct thread_info, kgdb_thread_info);
 static int kgdb_singlestep(struct pt_regs *regs)
 {
@@ -181,7 +182,7 @@ static int kgdb_singlestep(struct pt_regs *regs)
 
 	kgdb_handle_exception(0, SIGTRAP, 0, regs);
 
-	if (thread_info != exception_thread_info)
+	if ((thread_info != exception_thread_info) && (!check_irq_replay()))
 		/* Restore current_thread_info lastly. */
 		memcpy(exception_thread_info, backup_current_thread_info, sizeof *thread_info);
 
