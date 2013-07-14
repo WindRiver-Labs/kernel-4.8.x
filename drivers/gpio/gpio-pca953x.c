4ff0e9551c757d2c7199b3c98f9c1ac3fe5a4a6b
Shawn Guo <shawn.guo@freescale.com>
2013-07-14 21:52:38 +0800

    MLK-11293: gpio: pca953x: add device_reset() call
    commit dcf2d076fc42929bb93e4377f65aea9ef2dad03b from
    git://git.freescale.com/imx/linux-2.6-imx.git imx_4.1.15_1.0.0_ga
    
    The pca953x type of devices, e.g. max7310, may have a reset which needs
    to be handled to get the device start working.  Add a device_reset()
    call for that, and defer the probe if the reset controller for that is
    not ready yet.
    
    Signed-off-by: Shawn Guo <shawn.guo@freescale.com>
    Signed-off-by: Fugang Duan <B38611@freescale.com>
    (cherry picked from commit: d3264091b30b777d94a18efda9823a06668d5b10)

--- drivers/gpio/gpio-pca953x.c
+++ drivers/gpio/gpio-pca953x.c
@@ -17,6 +17,7 @@
 #include <linux/interrupt.h>
 #include <linux/i2c.h>
 #include <linux/platform_data/pca953x.h>
+#include <linux/reset.h>
 #include <linux/slab.h>
 #include <asm/unaligned.h>
 #include <linux/of_platform.h>
@@ -787,6 +788,10 @@ static int pca953x_probe(struct i2c_client *client,
 	chip->chip_type = PCA_CHIP_TYPE(chip->driver_data);
 
 	mutex_init(&chip->i2c_lock);
+
+	ret = device_reset(&client->dev);
+	if (ret == -ENODEV)
+		return -EPROBE_DEFER;
 
 	/* initialize cached registers from their original values.
 	 * we can't share this chip with another i2c master.
