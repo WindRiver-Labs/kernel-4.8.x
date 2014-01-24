c7a0dd8b7bc89eb077defb8b6c5f2944d34e4c85
Michal Simek <michal.simek@xilinx.com>
2014-01-24 13:24:51 +0100

    spi: zynq-qspi: Remove additional empty lines
    No functional code change
    
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/spi/spi-zynq-qspi.c
+++ drivers/spi/spi-zynq-qspi.c
@@ -1136,7 +1136,6 @@ static int zynq_qspi_probe(struct platform_device *pdev)
 	if (clk_notifier_register(xqspi->devclk, &xqspi->clk_rate_change_nb))
 		dev_warn(&pdev->dev, "Unable to register clock notifier.\n");
 
-
 	/* QSPI controller initializations */
 	zynq_qspi_init_hw(xqspi->regs, xqspi->is_dual);
 
@@ -1231,7 +1230,6 @@ static int zynq_qspi_remove(struct platform_device *pdev)
 
 	spi_unregister_master(master);
 	spi_master_put(master);
-
 
 	dev_dbg(&pdev->dev, "remove succeeded\n");
 	return 0;
