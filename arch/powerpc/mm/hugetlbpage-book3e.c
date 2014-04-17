8c9920d78ac94a175dc182ae107ce850f128e9ad
Mihai Caraman <mihai.caraman@freescale.com>
2014-04-17 12:41:09 +0300

    powerpc/booke3e: Move tlb lock/unlock functions
    Move book3e_tlb_lock/unlock functions from hugetlbpages to mm to be
    used from other kernel places.
    
    Signed-off-by: Mihai Caraman <mihai.caraman@freescale.com>
    (cherry picked and merged from sdk1.5 commit c84bbd80aebb40de4fcc7894759dfff8857d405c)
    Change-Id: I89239824880e81c8257fe612cad903eebbb5adbe
    Reviewed-on: http://git.am.freescale.net:8181/11559
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Stuart Yoder <stuart.yoder@freescale.com>
    Reviewed-by: Jose Rivera <German.Rivera@freescale.com>
    (cherry picked from commit 5f3005e9f6cbd4d01597407aa8a7aa5a2f5a381c)
    Signed-off-by: Scott Wood <scott.wood@nxp.com>
    [Original patch taken from QorIQ-SDK-V2.0-20160527-yocto]
    Signed-off-by: Yanjiang Jin <yanjiang.jin@windriver.com>

--- arch/powerpc/mm/hugetlbpage-book3e.c
+++ arch/powerpc/mm/hugetlbpage-book3e.c
@@ -53,59 +53,6 @@ static inline int mmu_get_tsize(int psize)
 	return mmu_psize_defs[psize].enc;
 }
 
-#if defined(CONFIG_PPC_FSL_BOOK3E) && defined(CONFIG_PPC64)
-#include <asm/paca.h>
-
-static inline void book3e_tlb_lock(void)
-{
-	struct paca_struct *paca = get_paca();
-	unsigned long tmp;
-	int token = smp_processor_id() + 1;
-
-	/*
-	 * Besides being unnecessary in the absence of SMT, this
-	 * check prevents trying to do lbarx/stbcx. on e5500 which
-	 * doesn't implement either feature.
-	 */
-	if (!cpu_has_feature(CPU_FTR_SMT))
-		return;
-
-	asm volatile("1: lbarx %0, 0, %1;"
-		     "cmpwi %0, 0;"
-		     "bne 2f;"
-		     "stbcx. %2, 0, %1;"
-		     "bne 1b;"
-		     "b 3f;"
-		     "2: lbzx %0, 0, %1;"
-		     "cmpwi %0, 0;"
-		     "bne 2b;"
-		     "b 1b;"
-		     "3:"
-		     : "=&r" (tmp)
-		     : "r" (&paca->tcd_ptr->lock), "r" (token)
-		     : "memory");
-}
-
-static inline void book3e_tlb_unlock(void)
-{
-	struct paca_struct *paca = get_paca();
-
-	if (!cpu_has_feature(CPU_FTR_SMT))
-		return;
-
-	isync();
-	paca->tcd_ptr->lock = 0;
-}
-#else
-static inline void book3e_tlb_lock(void)
-{
-}
-
-static inline void book3e_tlb_unlock(void)
-{
-}
-#endif
-
 static inline int book3e_tlb_exists(unsigned long ea, unsigned long pid)
 {
 	int found = 0;
