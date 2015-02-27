c8e8d7f4f402f0aedb5420206b0667552253e096
Peter Chen <peter.chen@freescale.com>
2015-02-27 13:28:38 +0800

    MLK-10282-4 usb: chipidea: usbmisc_imx: add imx7d support
    commit afe3b53843d413b57acae6942248ad5de1e6088b from
    git://git.freescale.com/imx/linux-2.6-imx.git imx_4.1.15_1.0.0_ga
    
    The layout is the same with vybrid, each controller has its
    own non-core region.
    
    Signed-off-by: Peter Chen <peter.chen@freescale.com>
    (cherry picked from commit adc52ade1a10a8b33885d52b4338b21f61446d3c)
    [Delete redundant part of upstream]
    Signed-off-by: Quanyang Wang <quanyang.wang@windriver.com>

--- drivers/usb/chipidea/usbmisc_imx.c
+++ drivers/usb/chipidea/usbmisc_imx.c
@@ -868,6 +868,10 @@ static const struct of_device_id usbmisc_imx_dt_ids[] = {
 		.compatible = "fsl,imx6ul-usbmisc",
 		.data = &imx6sx_usbmisc_ops,
 	},
+	{
+		.compatible = "fsl,imx7d-usbmisc",
+		.data = &imx7d_usbmisc_ops,
+	},
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, usbmisc_imx_dt_ids);
