d8199fe4fcc8bca3c83b773cd10ae9b66b80d0ce
Fugang Duan <b38611@freescale.com>
2015-03-04 15:26:59 +0800

    MLK-10367 tty: serial: imx: set uart module clock to 80Mhz
    commit ae3daf8d362a0c1d0978672e33b729cce6cbd277 from
    git://git.freescale.com/imx/linux-2.6-imx.git imx_4.1.15_1.0.0_ga
    
    When UART module clock is great than 80Mhz, there may have risk after
    confirming with IC owner. So set the maximum module clock to 80Mhz.
    
    Signed-off-by: Fugang Duan <B38611@freescale.com>
    (cherry picked from commit: 330a1245cb91583d9bc916bbb6d8c7d2c86b26f3)

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -185,6 +185,7 @@
 #define DRIVER_NAME "IMX-uart"
 
 #define UART_NR 8
+#define IMX_MODULE_MAX_CLK_RATE	80000000
 
 /* i.MX21 type uart runs on all i.mx except i.MX1 and i.MX6q */
 enum imx_uart_type {
@@ -2074,6 +2075,8 @@ static int serial_imx_probe(struct platform_device *pdev)
 		return ret;
 	}
 
+	if (sport->clk_per > IMX_MODULE_MAX_CLK_RATE)
+		clk_set_rate(sport->clk_per, IMX_MODULE_MAX_CLK_RATE);
 	sport->port.uartclk = clk_get_rate(sport->clk_per);
 
 	/* For register access, we only need to enable the ipg clock. */
