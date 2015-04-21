cf9508a8aa6b3f712f8f3b0320768d3afcbe6e94
J. German Rivera <German.Rivera@freescale.com>
2015-04-21 11:33:02 -0500

    staging: fsl-mc: Fix missing cases for for interrupts not supported
    When DPMCP interrupt support was added, I forgot to consider cases
    in which support without interrupts is needed.
    
    Signed-off-by: J. German Rivera <German.Rivera@freescale.com>
    Change-Id: I2575daba67c76a2a1d6cc495cbaf94d03acf470d
    Reviewed-on: http://git.am.freescale.net:8181/35383
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Bharat Bhushan <Bharat.Bhushan@freescale.com>
    Reviewed-by: Stuart Yoder <stuart.yoder@freescale.com>
    [Original patch taken from QorIQ-SDK-V2.0-20160527-yocto]
    Signed-off-by: Yanjiang Jin <yanjiang.jin@windriver.com>

--- drivers/staging/fsl-mc/bus/mc-sys.c
+++ drivers/staging/fsl-mc/bus/mc-sys.c
@@ -235,6 +235,9 @@ int fsl_mc_io_setup_dpmcp_irq(struct fsl_mc_io *mc_io)
 	if (WARN_ON(!dpmcp_dev))
 		return -EINVAL;
 
+	if (WARN_ON(!fsl_mc_interrupts_supported()))
+		return -EINVAL;
+
 	if (WARN_ON(dpmcp_dev->obj_desc.irq_count != 1))
 		return -EINVAL;
 
@@ -278,6 +281,8 @@ static void teardown_dpmcp_irq(struct fsl_mc_io *mc_io)
 	struct fsl_mc_device *dpmcp_dev = mc_io->dpmcp_dev;
 
 	if (WARN_ON(!dpmcp_dev))
+		return;
+	if (WARN_ON(!fsl_mc_interrupts_supported()))
 		return;
 	if (WARN_ON(!dpmcp_dev->irqs))
 		return;
@@ -355,7 +360,8 @@ int __must_check fsl_create_mc_io(struct device *dev,
 		if (error < 0)
 			goto error_destroy_mc_io;
 
-		if (!(flags & FSL_MC_IO_ATOMIC_CONTEXT_PORTAL)) {
+		if (!(flags & FSL_MC_IO_ATOMIC_CONTEXT_PORTAL) &&
+			fsl_mc_interrupts_supported()) {
 			error = fsl_mc_io_setup_dpmcp_irq(mc_io);
 			if (error < 0)
 				goto error_destroy_mc_io;
