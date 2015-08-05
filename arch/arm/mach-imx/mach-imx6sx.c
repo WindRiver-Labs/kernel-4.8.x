364f16cf8cf037fe61f725396ca9f579b7dbe336
Bai Ping <b51503@freescale.com>
2015-08-06 00:27:47 +0800

    MLK-11322-02 ARM: imx: add io map for pm code
    commit 1011b716daf023e7e73648356486ae66781b5bc7 from
    git://git.freescale.com/imx/linux-2.6-imx.git imx_4.1.15_1.0.0_ga
    
    add necessary io memory mapping used by power management.
    
    Signed-off-by: Bai Ping <b51503@freescale.com>

--- arch/arm/mach-imx/mach-imx6sx.c
+++ arch/arm/mach-imx/mach-imx6sx.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Freescale Semiconductor, Inc.
+ * Copyright 2014-2015 Freescale Semiconductor, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -106,12 +106,19 @@ static void __init imx6sx_init_late(void)
 		platform_device_register_simple("imx6q-cpufreq", -1, NULL, 0);
 }
 
+static void __init imx6sx_map_io(void)
+{
+	debug_ll_io_init();
+	imx6_pm_map_io();
+}
+
 static const char * const imx6sx_dt_compat[] __initconst = {
 	"fsl,imx6sx",
 	NULL,
 };
 
 DT_MACHINE_START(IMX6SX, "Freescale i.MX6 SoloX (Device Tree)")
+	.map_io		= imx6sx_map_io,
 	.l2c_aux_val 	= 0,
 	.l2c_aux_mask	= ~0,
 	.init_irq	= imx6sx_init_irq,
