1b030278ca6120207cc2353fc2d71cd8e308c2b2
Magnus Karlsson <magnus.karlsson@intel.com>
2015-09-24 19:15:47 +0300

    Revert "arch/arm/mach-axxia: Fixed affinity error on IRQ migration"
    This is a port of commit 9d49f26723fde32e1 from
    linux-yocto-3.10, branch standard/axxia/base, that reverts
    d802a3a68400066a6 from the same branch.
    
    Signed-off-by: Magnus Karlsson <magnus.karlsson@intel.com>

--- arch/arm/mach-axxia/include/mach/axxia-gic.h
+++ arch/arm/mach-axxia/include/mach/axxia-gic.h
@@ -12,6 +12,5 @@ void axxia_gic_raise_softirq(const struct cpumask *mask, unsigned int irq);
 void axxia_gic_secondary_init(void);
 int __init axxia_gic_of_init(struct device_node *node,
 			     struct device_node *parent);
-void axxia_gic_dump_mask(char *tmp, const struct cpumask *mask);
-void axxia_gic_kill_cpu(u32 rcpu);
+
 #endif
