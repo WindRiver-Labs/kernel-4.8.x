946c5b10cedd7b57ad272397ed507f2b200b4424
Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
2015-11-18 20:55:45 +0530

    media: xilinx: Refactor common HLS defines
    Trivial code refactoring.
    Common defines will be used by other HLS drivers.
    
    Signed-off-by: Radhey Shyam Pandey <radheys@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/media/platform/xilinx/xilinx-hls.c
+++ drivers/media/platform/xilinx/xilinx-hls.c
@@ -20,26 +20,8 @@
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-subdev.h>
 
-#include "xilinx-controls.h"
+#include "xilinx-hls-common.h"
 #include "xilinx-vip.h"
-
-#define XHLS_DEF_WIDTH				1920
-#define XHLS_DEF_HEIGHT				1080
-
-#define XHLS_REG_CTRL_DONE			(1 << 1)
-#define XHLS_REG_CTRL_IDLE			(1 << 2)
-#define XHLS_REG_CTRL_READY			(1 << 3)
-#define XHLS_REG_CTRL_AUTO_RESTART		(1 << 7)
-#define XHLS_REG_GIE				0x04
-#define XHLS_REG_GIE_GIE			(1 << 0)
-#define XHLS_REG_IER				0x08
-#define XHLS_REG_IER_DONE			(1 << 0)
-#define XHLS_REG_IER_READY			(1 << 1)
-#define XHLS_REG_ISR				0x0c
-#define XHLS_REG_ISR_DONE			(1 << 0)
-#define XHLS_REG_ISR_READY			(1 << 1)
-#define XHLS_REG_ROWS				0x10
-#define XHLS_REG_COLS				0x18
 
 /**
  * struct xhls_device - Xilinx HLS Core device structure
@@ -430,7 +412,7 @@ static int xhls_probe(struct platform_device *pdev)
 	xhls->pads[XVIP_PAD_SINK].flags = MEDIA_PAD_FL_SINK;
 	xhls->pads[XVIP_PAD_SOURCE].flags = MEDIA_PAD_FL_SOURCE;
 	subdev->entity.ops = &xhls_media_ops;
-	ret = media_entity_init(&subdev->entity, 2, xhls->pads, 0);
+	ret = media_entity_pads_init(&subdev->entity, 2, xhls->pads);
 	if (ret < 0)
 		goto error;
 
