1084fea3561c2575712b8baf07a620d6e3e90f0c
czou <cao.zou@windriver.com>
2015-12-03 16:09:44 +0800

    ls1021: add the support of RTLswitch for fixed-link property
    RTLswitch phy not connected to the real MDIO bus, set the dts
    fixed-link property of phy of RTLswitch, and create the fixed-link
    phy in mach-ls1021.
    
    Signed-off-by: czou <cao.zou@windriver.com>
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- arch/arm/mach-imx/mach-ls1021a.c
+++ arch/arm/mach-imx/mach-ls1021a.c
@@ -8,8 +8,41 @@
  */
 
 #include <asm/mach/arch.h>
-
+#include <linux/of_platform.h>
+#include <linux/phy.h>
+#include <linux/phy_fixed.h>
 #include "common.h"
+
+#ifdef CONFIG_FIXED_PHY
+static int __init of_add_fixed_phys(void)
+{
+	int ret;
+	struct device_node *np;
+	u32 *fixed_link;
+	struct fixed_phy_status status = {};
+
+	for_each_node_by_name(np, "ethernet") {
+		fixed_link  = (u32 *)of_get_property(np, "fixed-link", NULL);
+		if (!fixed_link)
+			continue;
+
+		status.link = 1;
+		status.duplex = be32_to_cpu(fixed_link[1]);
+		status.speed = be32_to_cpu(fixed_link[2]);
+		status.pause = be32_to_cpu(fixed_link[3]);
+		status.asym_pause = be32_to_cpu(fixed_link[4]);
+
+		ret = fixed_phy_add(PHY_POLL, be32_to_cpu(fixed_link[0]), &status, -1);
+		if (ret) {
+			of_node_put(np);
+			return ret;
+		}
+	}
+
+	return 0;
+}
+arch_initcall(of_add_fixed_phys);
+#endif /* CONFIG_FIXED_PHY */
 
 static const char * const ls1021a_dt_compat[] __initconst = {
 	"fsl,ls1021a",
