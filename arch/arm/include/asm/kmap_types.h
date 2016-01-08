0823b59e2c8a6803789e74cedfe59a07de7c20f5
John Jacques <john.jacques@intel.com>
2016-01-08 17:51:38 +0200

    arch/arm: Fix Compiler Warnings
    The Axxia updates used NR_CPUS instead of CONFIG_NR_CPUS.
    
    Signed-off-by: John Jacques <john.jacques@intel.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

--- arch/arm/include/asm/kmap_types.h
+++ arch/arm/include/asm/kmap_types.h
@@ -4,7 +4,7 @@
 /*
  * This is the "bare minimum".  AIO seems to require this.
  */
-#if (NR_CPUS > 15)
+#if (CONFIG_NR_CPUS > 15)
 /* Prevent overlap between fixmap mapping and CPU vector page for 16th core */
 #define KM_TYPE_NR 15
 #else
