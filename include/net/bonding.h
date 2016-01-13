60102dce4de8546a301b6f860911df5ad6ebfccb
Jianhua Xie <jianhua.xie@nxp.com>
2016-01-13 19:51:41 +0800

    bonding: add sdk-1.9 lag codes to sdk-2.0
    new file: include/net/hw_distribution.h
    new file: drivers/net/bonding/Kconfig
    new file: drivers/net/bonding/hw_distribution.c
    new file: drivers/net/bonding/hw_oh_pcd.c
    modified: drivers/net/Kconfig
    modified: drivers/net/bonding/Makefile
    modified: drivers/net/bonding/bond_main.c
    modified: drivers/net/bonding/bond_netlink.c
    modified: drivers/net/bonding/bond_sysfs.c
    modified: include/net/bonding.h
    
    Signed-off-by: Jianhua Xie <jianhua.xie@nxp.com>
    [Original patch taken from QorIQ-SDK-V2.0-20160527-yocto]
    Signed-off-by: Yanjiang Jin <yanjiang.jin@windriver.com>

--- include/net/bonding.h
+++ include/net/bonding.h
@@ -30,6 +30,10 @@
 #include <net/bond_alb.h>
 #include <net/bond_options.h>
 
+#ifdef CONFIG_HW_DISTRIBUTION_WITH_OH
+#include <net/hw_distribution.h>
+#endif
+
 #define BOND_MAX_ARP_TARGETS	16
 
 #define BOND_DEFAULT_MIIMON	100
@@ -139,6 +143,10 @@ struct bond_params {
 	int packets_per_slave;
 	int tlb_dynamic_lb;
 	struct reciprocal_value reciprocal_packets_per_slave;
+#ifdef CONFIG_HW_DISTRIBUTION_WITH_OH
+	struct oh_port_priv *ohp;
+	struct rtnl_link_stats64 oh_stats;
+#endif
 	u16 ad_actor_sys_prio;
 	u16 ad_user_port_key;
 
