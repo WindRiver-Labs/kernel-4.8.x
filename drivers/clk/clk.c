61ff5b7df3be69e9b28b9ebabb19fe604f7e334b
Scott Wood <scottwood@freescale.com>
2016-01-15 07:34:31 +0000

    clk: Add consumer APIs for discovering possible parent clocks
    Commit fc4a05d4b0eb ("clk: Remove unused provider APIs") removed
    __clk_get_num_parents() and clk_hw_get_parent_by_index(), leaving only
    true provider API versions that operate on struct clk_hw.
    
    qoriq-cpufreq needs these functions in order to determine the options
    it has for calling clk_set_parent() and thus populate the cpufreq
    table, so revive them as legitimate consumer APIs.
    
    [This is a backport of a patch submitted upstream; in 4.1 we only
    need clk_get_num_parents()]
    
    Signed-off-by: Scott Wood <scottwood@freescale.com>
    [Original patch taken from QorIQ-SDK-V2.0-20160527-yocto]
    Signed-off-by: Yanjiang Jin <yanjiang.jin@windriver.com>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -2057,6 +2057,12 @@ static const struct file_operations clk_summary_fops = {
 	.release	= single_release,
 };
 
+unsigned int clk_get_num_parents(struct clk *clk)
+{
+	return !clk ? 0 : clk->core->num_parents;
+}
+EXPORT_SYMBOL_GPL(clk_get_num_parents);
+
 static void clk_dump_one(struct seq_file *s, struct clk_core *c, int level)
 {
 	if (!c)
@@ -2112,7 +2118,6 @@ static int clk_dump(struct seq_file *s, void *data)
 	seq_puts(s, "}\n");
 	return 0;
 }
-
 
 static int clk_dump_open(struct inode *inode, struct file *file)
 {
