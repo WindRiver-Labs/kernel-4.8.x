0080544365929b8b7412108e7a6d55186d7d6215
Panwar, Ashish <ashish.panwar@intel.com>
2016-01-20 19:17:16 +0530

    ASoC: Intel: Skylake: IPC for enabling/disabling firmware logging.
    commit c517f32671c46c10f5b60ff0d61d2468351f7455 from
    git://git@github.com:01org/linux-intel-4.9.git
    
    An API for enabling/disabling firmware logging from the driver.
    Currently, the default log priority is set to verbose (6). Log levels
    will be added in drivers in the next update.
    
    Change-Id: Idf2c6232d0654e1e6e94331fb5535e272d86d846
    Signed-off-by: Panwar, Ashish <ashish.panwar@intel.com>
    Reviewed-on: https://git-gar-1.devtools.intel.com/gerrit/8050
    Reviewed-by: Babu, Ramesh <ramesh.babu@intel.com>
    Tested-by: Babu, Ramesh <ramesh.babu@intel.com>
    [Kevin: Just some minor context mods in order to port to wrlinux]
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

--- sound/soc/intel/skylake/skl-sst-ipc.h
+++ sound/soc/intel/skylake/skl-sst-ipc.h
@@ -159,6 +159,16 @@ struct skl_ipc_d0ix_msg {
 	u8 wake;
 };
 
+struct skl_log_state {
+	u32	enable;
+	u32	priority;
+};
+
+struct skl_log_state_msg {
+	u32	core_mask;
+	struct	skl_log_state logs_core[2];
+};
+
 #define SKL_IPC_BOOT_MSECS		3000
 
 #define SKL_IPC_D3_MASK	0
@@ -207,6 +217,8 @@ int skl_ipc_check_D0i0(struct sst_dsp *dsp, bool state);
 
 int skl_sst_ipc_load_library(struct sst_generic_ipc *ipc,
 			u8 dma_id, u8 table_id, bool wait);
+
+int skl_dsp_enable_logging(struct sst_generic_ipc *ipc, int core, int enable);
 
 void skl_ipc_int_enable(struct sst_dsp *dsp);
 void skl_ipc_op_int_enable(struct sst_dsp *ctx);
