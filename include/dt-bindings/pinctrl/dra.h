ed84d4629dedb29c2a12886f70e998c72079994b
Nishanth Menon <nm@ti.com>
2016-03-17 05:11:37 -0500

    pinctrl: Introduce TI IOdelay configuration driver
    This patch comes from:
      git://git.ti.com/processor-sdk/processor-sdk-linux.git
    
    SoC family such as DRA7 family of processors have, in addition
    to the regular muxing of pins (as done by pinctrl-single), an
    additional hardware module called IODelay which is also expected to be
    configured. This "IODelay" module has it's own register space that is
    independent of the control module.
    
    It is advocated strongly in TI's official documentation considering
    the existing design of the DRA7 family of processors during mux or
    IODelay reconfiguration, there is a potential for a significant glitch
    which may cause functional impairment to certain hardware. It is
    hence recommended to do as little of muxing as absolutely necessary
    without I/O isolation (which can only be done in initial stages of
    bootloader).
    
    NOTE: with the system wide I/O isolation scheme present in DRA7 SoC
    family, it is not reasonable to do stop all I/O operations for every
    such pad configuration scheme. So, we will let it glitch when used in
    this mode.
    
    Even with the above limitation, certain functionality such as MMC has
    mandatory need for IODelay reconfiguration requirements, depending on
    speed of transfer. In these cases, with careful examination of usecase
    involved, the expected glitch can be controlled such that it does not
    impact functionality.
    
    In short, IODelay module support as a padconf driver being introduced
    here is not expected to do SoC wide I/O Isolation and is meant for
    a limited subset of IODelay configuration requirements that need to
    be dynamic and whose glitchy behavior will not cause functionality
    failure for that interface.
    
    NOTE: this approach has already been rejected in upstream:
    http://marc.info/?t=142542741200007&r=1&w=2 However, upstream synced
    series involves core framework changes that are out of immediate scope
    atm.
    
    IMPORTANT NOTE: we take the approach of keeping LOCK_BITs cleared
    to 0x0 at all times, even when configuring Manual IO Timing Modes.
    This is done by eliminating the LOCK_BIT=1 setting from Step
    of the Manual IO timing Mode configuration procedure. This option
    leaves the CFG_* registers unprotected from unintended writes to the
    CTRL_CORE_PAD_* registers while Manual IO Timing Modes are configured.
    
    This approach is taken to allow for a generic driver to exist in kernel
    world that has to be used carefully in required usecases.
    
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
    (cherry picked from commit 52a607e7c45e44e09f50233384cc352417556966)
    Signed-off-by: Zumeng Chen <zumeng.chen@windriver.com>

--- include/dt-bindings/pinctrl/dra.h
+++ include/dt-bindings/pinctrl/dra.h
@@ -49,6 +49,7 @@
 #define MUX_VIRTUAL_MODE15	(MODE_SELECT | (0xf << 4))
 
 #define MODE_SELECT		(1 << 8)
+#define MANUAL_MODE		MODE_SELECT
 
 #define PULL_ENA		(0 << 16)
 #define PULL_DIS		(1 << 16)
@@ -66,6 +67,10 @@
 #define PIN_INPUT_SLEW		(INPUT_EN | SLEWCONTROL)
 #define PIN_INPUT_PULLUP	(PULL_ENA | INPUT_EN | PULL_UP)
 #define PIN_INPUT_PULLDOWN	(PULL_ENA | INPUT_EN)
+
+/* DRA7 IODELAY configuration parameters */
+#define A_DELAY(val)		((val) & 0xFFFF)
+#define G_DELAY(val)		(((val) & 0xFFFF) << 16)
 
 /*
  * Macro to allow using the absolute physical address instead of the
