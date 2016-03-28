4ad9ce77cfb5c3730bca9b01ced335edc2b5c3b6
Han Xu <han.xu@nxp.com>
2016-03-28 10:35:59 -0500

    MLK-12601: mtd: gpmi: provide the option to use legacy bch geometry
    commit 5102a01f1aee8b53faa874dd6f54e69d4b7dd624 from
    git://git.freescale.com/imx/linux-2.6-imx.git imx_4.1.15_1.0.0_ga
    
    Provide an option in DT to use legacy bch geometry, which compatible
    with the 3.10 kernel bch setting. To enable the feature, adding
    "fsl,legacy-bch-geometry" under gpmi-nand node.
    
    NOTICE: The feature must be enabled/disabled in both u-boot and kernel.
    
    Conflicts:
    	drivers/mtd/nand/gpmi-nand/gpmi-nand.h
    
    Signed-off-by: Han Xu <han.xu@nxp.com>
    (cherry picked from commit 4d28b1693905526558892d40525763e6bc4469e4)

--- drivers/mtd/nand/gpmi-nand/gpmi-nand.c
+++ drivers/mtd/nand/gpmi-nand/gpmi-nand.c
@@ -583,8 +583,9 @@ int common_nfc_set_geometry(struct gpmi_nand_data *this)
 		return -EINVAL;
 	}
 
-	if (!(chip->ecc_strength_ds > 0 && chip->ecc_step_ds > 0) &&
-			!(mtd->oobsize > 1024))
+	if ((!(chip->ecc_strength_ds > 0 && chip->ecc_step_ds > 0) &&
+			(mtd->oobsize < 1024)) || this->legacy_bch_geometry)
+		dev_warn(this->dev, "use legacy bch geometry\n");
 		return legacy_set_geometry(this);
 
 	if (mtd->oobsize > 1024 || chip->ecc_step_ds < mtd->oobsize)
@@ -2312,6 +2313,9 @@ static int gpmi_nand_init(struct gpmi_nand_data *this)
 
 	if (chip->bbt_options & NAND_BBT_USE_FLASH) {
 		chip->bbt_options |= NAND_BBT_NO_OOB;
+	if (of_property_read_bool(this->dev->of_node,
+				"fsl,legacy-bch-geometry"))
+		this->legacy_bch_geometry = true;
 
 		if (of_property_read_bool(this->dev->of_node,
 						"fsl,no-blockmark-swap"))
