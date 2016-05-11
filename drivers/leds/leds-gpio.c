d80236561a54e9423c3fde78b87cfcc01f554315
Hebbar, Gururaja <gururaja.hebbar@ti.com>
2016-05-11 10:17:38 -0500

    leds: leds-gpio: Enhance pinctrl support
    This patch comes from:
      git://git.ti.com/processor-sdk/processor-sdk-linux.git
    
    update leds-gpio driver to set the state of the pins to:
    
    - "sleep" on suspend
    - "default" on resume
    
    By optionally putting the pins into sleep state in the suspend callback
    we can accomplish two things.
    - minimize current leakage from pins and thus save power,
    - prevent the IP from driving pins output in an uncontrolled manner,
      which may happen if the power domain drops the domain regulator.
    
    suspend()/resume() callbacks are added which simply puts the pins to
     sleep/default state.
    
    Signed-off-by: Hebbar, Gururaja <gururaja.hebbar@ti.com>
    Signed-off-by: Dave Gerlach <d-gerlach@ti.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>
    (cherry picked from commit e92e9406d92c2560ad304d882a8b72336e8b9f71)
    Signed-off-by: Zumeng Chen <zumeng.chen@windriver.com>

--- drivers/leds/leds-gpio.c
+++ drivers/leds/leds-gpio.c
@@ -20,6 +20,7 @@
 #include <linux/platform_device.h>
 #include <linux/property.h>
 #include <linux/slab.h>
+#include <linux/pinctrl/consumer.h>
 
 struct gpio_led_data {
 	struct led_classdev cdev;
@@ -290,6 +291,26 @@ static void gpio_led_shutdown(struct platform_device *pdev)
 	}
 }
 
+#ifdef CONFIG_PM_SLEEP
+static int gpio_led_suspend(struct device *dev)
+{
+	/* Select sleep pin state */
+	pinctrl_pm_select_sleep_state(dev);
+
+	return 0;
+}
+
+static int gpio_led_resume(struct device *dev)
+{
+	/* Select default pin state */
+	pinctrl_pm_select_default_state(dev);
+
+	return 0;
+}
+#endif
+
+static SIMPLE_DEV_PM_OPS(gpio_led_pm_ops, gpio_led_suspend, gpio_led_resume);
+
 static struct platform_driver gpio_led_driver = {
 	.probe		= gpio_led_probe,
 	.remove		= gpio_led_remove,
@@ -297,6 +318,7 @@ static struct platform_driver gpio_led_driver = {
 	.driver		= {
 		.name	= "leds-gpio",
 		.of_match_table = of_gpio_leds_match,
+		.pm	= &gpio_led_pm_ops,
 	},
 };
 
