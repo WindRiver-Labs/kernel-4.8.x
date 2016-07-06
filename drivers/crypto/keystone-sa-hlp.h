80c9999ab360db8b85d1ea0e1b2aa7ef8316b8ff
Vitaly Andrianov <vitalya@ti.com>
2016-07-06 14:07:13 -0400

    crypto: ks2: add dma resources allocation code
    This patch comes from:
      git://git.ti.com/processor-sdk/processor-sdk-linux.git
    
    Keystone crypto accelerator uses KS2 HW queues and dma channels for its
    operation. This commit adds code to allocate required memory and buffer
    pools, open necessary queues and dma channels. It also adds the code to
    release allocated resources and creates stub tasklets for TX and RX
    completion processes.
    
    Signed-off-by: Vitaly Andrianov <vitalya@ti.com>
    Signed-off-by: Jacob Stiffler <j-stiffler@ti.com>
    (cherry picked from commit f15f73870b1759717ec59920a92d42eca0ee088e)
    Signed-off-by: Zumeng Chen <zumeng.chen@windriver.com>

--- drivers/crypto/keystone-sa-hlp.h
+++ drivers/crypto/keystone-sa-hlp.h
@@ -19,8 +19,11 @@
 #ifndef _KEYSTONE_SA_HLP_
 #define _KEYSTONE_SA_HLP_
 
+#include <linux/interrupt.h>
 #include <linux/soc/ti/knav_dma.h>
 #include <linux/regmap.h>
+
+#define SA_RX_BUF0_SIZE 1500
 
 #define SA_PID_OFS		0
 #define SA_CMD_STATUS_OFS	0x8
@@ -33,7 +36,18 @@
 struct keystone_crypto_data {
 	struct platform_device	*pdev;
 	struct clk		*clk;
+	struct tasklet_struct	rx_task;
+	struct tasklet_struct	tx_task;
+	struct dma_pool		*sc_pool;
+	struct kmem_cache	*dma_req_ctx_cache;
 	struct regmap	*sa_regmap;
+
+	void		*rx_chan;
+	void		*rx_fdq[KNAV_DMA_FDQ_PER_CHAN];
+	void		*rx_compl_q;
+	void		*tx_chan;
+	void		*tx_submit_q;
+	void		*tx_compl_q;
 	u32		tx_submit_qid;
 	u32		tx_compl_qid;
 	u32		rx_compl_qid;
@@ -43,8 +57,10 @@ struct keystone_crypto_data {
 	u32		rx_queue_depths[KNAV_DMA_FDQ_PER_CHAN];
 	u32		rx_pool_size;
 	u32		rx_pool_region_id;
+	void		*rx_pool;
 	u32		tx_pool_size;
 	u32		tx_pool_region_id;
+	void		*tx_pool;
 
 	/* Security context data */
 	u16		sc_id_start;
@@ -52,6 +68,11 @@ struct keystone_crypto_data {
 	u16		sc_id;
 	atomic_t	rx_dma_page_cnt; /* N buf from 2nd pool available */
 	atomic_t	tx_dma_desc_cnt; /* Tx DMA desc-s available */
+};
+
+/* Tx DMA callback param */
+struct sa_dma_req_ctx {
+	struct keystone_crypto_data *dev_data;
 };
 
 extern struct device *sa_ks2_dev;
