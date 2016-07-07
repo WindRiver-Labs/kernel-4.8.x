0d87c97d236f62f0d807d77d9e3f554cf9df8ca9
Vitaly Andrianov <vitalya@ti.com>
2016-07-07 13:06:43 -0400

    crypto: ks2: read driver configuration parameters from dtb
    This patch comes from:
      git://git.ti.com/processor-sdk/processor-sdk-linux.git
    
    This commit adds configuration fields such tx and rx queue numbers,
    buffer sizes, etc. to the driver instance data structure.
    It also adds the function to read the corresponding parameters from dtb.
    
    Signed-off-by: Vitaly Andrianov <vitalya@ti.com>
    Signed-off-by: Jacob Stiffler <j-stiffler@ti.com>
    (cherry picked from commit b8e46009f948a5c06964c749fa33f2524e62f15e)
    Signed-off-by: Zumeng Chen <zumeng.chen@windriver.com>

--- drivers/crypto/keystone-sa-hlp.h
+++ drivers/crypto/keystone-sa-hlp.h
@@ -19,10 +19,39 @@
 #ifndef _KEYSTONE_SA_HLP_
 #define _KEYSTONE_SA_HLP_
 
+#include <linux/soc/ti/knav_dma.h>
+#include <linux/regmap.h>
+
+#define SA_PID_OFS		0
+#define SA_CMD_STATUS_OFS	0x8
+#define SA_PA_FLOWID_OFS	0x10
+#define SA_CDMA_FLOWID_OFS	0x14
+#define	SA_PA_ENG_ID_OFS	0x18
+#define	SA_CDMA_ENG_ID_OFS	0x1C
+
 /* Crypto driver instance data */
 struct keystone_crypto_data {
 	struct platform_device	*pdev;
 	struct clk		*clk;
+	struct regmap	*sa_regmap;
+	u32		tx_submit_qid;
+	u32		tx_compl_qid;
+	u32		rx_compl_qid;
+	const char	*rx_chan_name;
+	const char	*tx_chan_name;
+	u32		tx_queue_depth;
+	u32		rx_queue_depths[KNAV_DMA_FDQ_PER_CHAN];
+	u32		rx_pool_size;
+	u32		rx_pool_region_id;
+	u32		tx_pool_size;
+	u32		tx_pool_region_id;
+
+	/* Security context data */
+	u16		sc_id_start;
+	u16		sc_id_end;
+	u16		sc_id;
+	atomic_t	rx_dma_page_cnt; /* N buf from 2nd pool available */
+	atomic_t	tx_dma_desc_cnt; /* Tx DMA desc-s available */
 };
 
 extern struct device *sa_ks2_dev;
