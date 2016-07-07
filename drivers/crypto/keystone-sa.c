be182224774294bcbfa5270cb8f2ac4b06fdb0fa
Vitaly Andrianov <vitalya@ti.com>
2016-07-07 13:06:43 -0400

    crypto: ks2: read driver configuration parameters from dtb
    This patch comes from:
      git://git.ti.com/processor-sdk/processor-sdk-linux.git
    
    This commit adds configuration fields such tx and rx queue numbers,
    buffer sizes, etc. to the driver instance data structure.
    It also adds the function to read the corresponding parameters from dtb.
    
    Signed-off-by: Vitaly Andrianov <vitalya@ti.com>
    Signed-off-by: Jacob Stiffler <j-stiffler@ti.com>
    (cherry picked from commit b8e46009f948a5c06964c749fa33f2524e62f15e)
    Signed-off-by: Zumeng Chen <zumeng.chen@windriver.com>

--- drivers/crypto/keystone-sa.c
+++ drivers/crypto/keystone-sa.c
@@ -24,11 +24,116 @@
 #include <linux/init.h>
 #include <linux/slab.h>
 #include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
+#include <linux/regmap.h>
+#include <linux/mfd/syscon.h>
+#include <linux/soc/ti/knav_dma.h>
 #include "keystone-sa-hlp.h"
 
 struct device *sa_ks2_dev;
+
+static int sa_read_dtb(struct device_node *node,
+		       struct keystone_crypto_data *dev_data)
+{
+	int i, ret = 0;
+	struct device *dev = &dev_data->pdev->dev;
+	u32 temp[2];
+
+	ret = of_property_read_string(node, "ti,tx-channel",
+				      &dev_data->tx_chan_name);
+	if (ret < 0) {
+		dev_err(dev, "missing \"ti,tx-channel\" parameter\n");
+		return -EINVAL;
+	}
+
+	ret = of_property_read_u32(node, "ti,tx-queue-depth",
+				       &dev_data->tx_queue_depth);
+	if (ret < 0) {
+		dev_err(dev, "missing \"ti,tx-queue-depth\" parameter\n");
+		return -EINVAL;
+	}
+
+	atomic_set(&dev_data->tx_dma_desc_cnt, dev_data->tx_queue_depth);
+
+	ret = of_property_read_u32(node, "ti,tx-submit-queue",
+				       &dev_data->tx_submit_qid);
+	if (ret < 0) {
+		dev_err(dev, "missing \"ti,tx-submit-queue\" parameter\n");
+		return -EINVAL;
+	}
+
+	ret = of_property_read_u32(node, "ti,tx-completion-queue",
+				       &dev_data->tx_compl_qid);
+	if (ret < 0) {
+		dev_err(dev, "missing \"ti,tx-completion-queue\" parameter\n");
+		return -EINVAL;
+	}
+
+	ret = of_property_read_string(node, "ti,rx-channel",
+				      &dev_data->rx_chan_name);
+	if (ret < 0) {
+		dev_err(dev, "missing \"ti,rx-channel\" parameter\n");
+		return -EINVAL;
+	}
+
+	ret = of_property_read_u32_array(node, "ti,rx-queue-depth",
+					 dev_data->rx_queue_depths,
+					 KNAV_DMA_FDQ_PER_CHAN);
+	if (ret < 0) {
+		dev_err(dev, "missing \"ti,rx-queue-depth\" parameter\n");
+		return -EINVAL;
+	}
+	for (i = 0; i < KNAV_DMA_FDQ_PER_CHAN; i++)
+		dev_dbg(dev, "rx-queue-depth[%d]= %u\n", i,
+			dev_data->rx_queue_depths[i]);
+
+	atomic_set(&dev_data->rx_dma_page_cnt, 0);
+
+	ret = of_property_read_u32(node, "ti,rx-compl-queue",
+				       &dev_data->rx_compl_qid);
+	if (ret < 0) {
+		dev_err(dev, "missing \"ti,rx-compl-queue\" parameter\n");
+		return -EINVAL;
+	}
+
+	ret = of_property_read_u32_array(node, "ti,tx-pool", temp, 2);
+	if (ret < 0) {
+		dev_err(dev, "missing \"ti,tx-pool\" parameter\n");
+		return -EINVAL;
+	}
+	dev_data->tx_pool_size = temp[0];
+	dev_data->tx_pool_region_id = temp[1];
+
+	ret = of_property_read_u32_array(node, "ti,rx-pool", temp, 2);
+	if (ret < 0) {
+		dev_err(dev, "missing \"ti,rx-pool\" parameter\n");
+		return -EINVAL;
+	}
+	dev_data->rx_pool_size = temp[0];
+	dev_data->rx_pool_region_id = temp[1];
+
+	ret = of_property_read_u32_array(node, "ti,sc-id", temp, 2);
+	if (ret < 0) {
+		dev_err(dev, "missing \"ti,sc-id\" parameter\n");
+		return -EINVAL;
+	}
+	dev_data->sc_id_start = temp[0];
+	dev_data->sc_id_end = temp[1];
+	dev_data->sc_id = dev_data->sc_id_start;
+
+	dev_data->sa_regmap = syscon_regmap_lookup_by_phandle(node,
+							      "syscon-subsys");
+
+	if (IS_ERR(dev_data->sa_regmap)) {
+		dev_err(dev, "syscon_regmap_lookup_by_phandle failed\n");
+		return -EINVAL;
+	}
+
+	return 0;
+}
 
 static int keystone_crypto_remove(struct platform_device *pdev)
 {
@@ -41,6 +146,7 @@ static int keystone_crypto_remove(struct platform_device *pdev)
 static int keystone_crypto_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
+	struct device_node *node = pdev->dev.of_node;
 	struct keystone_crypto_data *dev_data;
 	int ret;
 
@@ -57,6 +163,13 @@ static int keystone_crypto_probe(struct platform_device *pdev)
 	if (ret < 0) {
 		dev_err(dev, "Failed to enable SA power-domain\n");
 		pm_runtime_disable(dev);
+		return ret;
+	}
+
+	/* Read configuration from device tree */
+	ret = sa_read_dtb(node, dev_data);
+	if (ret) {
+		dev_err(dev, "Failed to get all relevant configurations from DTB...\n");
 		return ret;
 	}
 
