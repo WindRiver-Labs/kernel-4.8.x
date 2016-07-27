72bb4ac5180723e7b69fddf2f5b570387d6b3a11
Anjali Singhai Jain <anjali.singhai@intel.com>
2016-07-27 12:02:30 -0700

    i40e: Fix a bug where a client close can be called before an open is complete
    commit 3a0f52928a0a06a7c4c735cd325ca646a1e40fae upstream.
    
    The client->open call in this path was not protected with the
    client instance mutex, and hence the client->close can get initiated
    before the open completes.
    
    Change-Id: I0ed60c38868dd3f44966b6ed49a063d0e5b7edf5
    Signed-off-by: Anjali Singhai Jain <anjali.singhai@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Liwei Song <liwei.song@windriver.com>

--- drivers/net/ethernet/intel/i40e/i40e_client.c
+++ drivers/net/ethernet/intel/i40e/i40e_client.c
@@ -559,6 +559,7 @@ void i40e_client_subtask(struct i40e_pf *pf)
 				 pf->hw.bus.device, pf->hw.bus.func);
 		}
 
+		mutex_lock(&i40e_client_instance_mutex);
 		/* Send an Open request to the client */
 		atomic_inc(&cdev->ref_cnt);
 		if (client->ops && client->ops->open)
@@ -572,6 +573,7 @@ void i40e_client_subtask(struct i40e_pf *pf)
 			atomic_dec(&client->ref_cnt);
 			continue;
 		}
+		mutex_unlock(&i40e_client_instance_mutex);
 	}
 	mutex_unlock(&i40e_client_mutex);
 }
