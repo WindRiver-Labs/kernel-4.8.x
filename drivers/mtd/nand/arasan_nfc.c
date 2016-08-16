e0b9f3402e3e3d8aacbdcdff531a70a4208eb338
Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
2016-08-16 14:48:44 +0530

    nand: arasan: balance the select and delect
    balance the select and de-select.
    this is to aid the power gating at deselect.
    
    Acked-by: Punnaiah Choudary Kalluri<punnaia@xilinx.com>
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- drivers/mtd/nand/arasan_nfc.c
+++ drivers/mtd/nand/arasan_nfc.c
@@ -854,6 +854,7 @@ static int anfc_init_timing_mode(struct anfc *nfc)
 		err = chip->onfi_set_features(mtd, chip,
 					ONFI_FEATURE_ADDR_TIMING_MODE,
 					(uint8_t *)&feature[0]);
+		chip->select_chip(mtd, -1);
 		if (err)
 			return err;
 	}
