de7cec391e722fec7e26b10a5b18df8ae6a0ecf0
Guneshwor Singh <guneshwor.o.singh@intel.com>
2016-08-22 12:13:36 +0530

    ASoC: Intel: CNL: Add library loading support
    commit 299f721ffa3223db94fb3a8a0a3bf0b27f2c17ae from
    git://git@github.com:01org/linux-intel-4.9.git
    
    Change-Id: I3a0d64da0eaef395cd5e6a95252e4025ce7921b2
    Signed-off-by: Guneshwor Singh <guneshwor.o.singh@intel.com>

--- sound/soc/intel/skylake/skl-sst-dsp.h
+++ sound/soc/intel/skylake/skl-sst-dsp.h
@@ -240,6 +240,7 @@ int skl_sst_init_fw(struct device *dev, struct skl_sst *ctx);
 int bxt_sst_init_fw(struct device *dev, struct skl_sst *ctx);
 void skl_sst_dsp_cleanup(struct device *dev, struct skl_sst *ctx);
 void bxt_sst_dsp_cleanup(struct device *dev, struct skl_sst *ctx);
+int bxt_load_library(struct sst_dsp *ctx, struct skl_lib_info *linfo, int lib_count);
 
 int snd_skl_parse_uuids(struct sst_dsp *ctx, unsigned int offset);
 int skl_get_pvt_id(struct skl_sst *ctx, uuid_le *uuid_mod, int instance_id);
