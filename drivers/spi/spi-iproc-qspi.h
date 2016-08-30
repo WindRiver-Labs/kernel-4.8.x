c23ae20bfa5f0074980541a1d18d5861b8e6b36c
Yendapally Reddy Dhananjaya Reddy <yendapally.reddy@broadcom.com>
2016-08-30 11:20:18 -0400

    SOC-4048 NS2:QSPI support
    NS2:QSPI support
    
    Bug: SOC-4048
    Change-Id: If172f87eb510c4950ce9e8c62885910545bfa1b7
    Signed-off-by: Yendapally Reddy Dhananjaya Reddy <yendapally.reddy@broadcom.com>
    Reviewed-on: http://gerrit-ccxsw.rtp.broadcom.com:8080/28472
    Reviewed-by: Randy Schacher <stuart.schacher@broadcom.com>
    Tested-by: Randy Schacher <stuart.schacher@broadcom.com>
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

---
+++ drivers/spi/spi-iproc-qspi.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016 Broadcom Ltd
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _QSPI_IPROC_H_
+#define _QSPI_IPROC_H_
+
+/*
+ * Shared Structure
+ */
+struct brcmspi_platform_data {
+	int			flash_cs;
+};
+
+#endif /* _SPI_IPROC_H_ */
