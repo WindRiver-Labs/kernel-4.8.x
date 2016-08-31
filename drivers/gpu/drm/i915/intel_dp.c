b1db050428627019e052e4af433b0fab3cb5431c
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2016-08-31 11:01:36 +0200

    drm/i915: Fix other intel_dp warnings too.
    commit 5423adf1d4c692ff300f9f4262e2d5241d4fe561 upstream
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -5073,7 +5073,7 @@ static void intel_dp_pps_init(struct drm_device *dev,
 
 /**
  * intel_dp_set_drrs_state - program registers for RR switch to take effect
- * @dev: DRM device
+ * @dev_priv: i915 device
  * @crtc_state: a pointer to the active intel_crtc_state
  * @refresh_rate: RR to be programmed
  *
@@ -5177,6 +5177,7 @@ static void intel_dp_set_drrs_state(struct drm_i915_private *dev_priv,
 /**
  * intel_edp_drrs_enable - init drrs struct if supported
  * @intel_dp: DP struct
+ * @crtc_state: A pointer to the active crtc state.
  *
  * Initializes frontbuffer_bits and drrs.dp
  */
@@ -5208,6 +5209,7 @@ unlock:
 /**
  * intel_edp_drrs_disable - Disable DRRS
  * @intel_dp: DP struct
+ * @old_crtc_state: Pointer to old crtc_state.
  *
  */
 void intel_edp_drrs_disable(struct intel_dp *intel_dp,
