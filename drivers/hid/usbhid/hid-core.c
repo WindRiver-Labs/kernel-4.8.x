454afb8aeccc6eaec99e4544c990ad4e83431f41
Kexin(Casey) Chen <Casey.Chen@windriver.com>
2016-09-21 16:14:45 -0700

    usb,keyboard,kdb: Implement HID keyboard polling
    This patch adds support to kdb for usb keyboards using the in kernel
    API for polling a specific device on a usb hcd.
    
    The usb hid-core requires two types of modifications:
    
      1) The hid-core must call back to kdb anytime a keyboard is attached
         or removed in order to allow kdb to know what keyboard devices
         should be polled.
    
      2) If the debugger_core is active the urb packet data needs to get
         sent directly to kdb.
    
    The modifications to kgdboc were simply to add the additional keyboard
    poll hook type as well as to call the function to flush usb queue when
    nearing the time to restart normal kernel execution.
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>
    CC: Greg Kroah-Hartman <gregkh@suse.de>
    CC: Alan Stern <stern@rowland.harvard.edu>
    CC: Dmitry Torokhov <dtor@mail.ru>
    
    Adjust hunk #2 #4 of drivers/hid/usbhid/hid-core.c
    
    Signed-off-by: He Zhe <zhe.he@windriver.com>
    
    Signed-off-by: Kexin(Casey) Chen <Casey.Chen@windriver.com>

--- drivers/hid/usbhid/hid-core.c
+++ drivers/hid/usbhid/hid-core.c
@@ -36,6 +36,8 @@
 #include <linux/hiddev.h>
 #include <linux/hid-debug.h>
 #include <linux/hidraw.h>
+#include <linux/kgdb.h>
+#include <linux/kdb.h>
 #include "usbhid.h"
 
 /*
@@ -279,9 +281,17 @@ static void hid_irq_in(struct urb *urb)
 			break;
 		usbhid_mark_busy(usbhid);
 		if (!test_bit(HID_RESUME_RUNNING, &usbhid->iofl)) {
-			hid_input_report(urb->context, HID_INPUT_REPORT,
-					 urb->transfer_buffer,
-					 urb->actual_length, 1);
+			if (unlikely(in_dbg_master() && hid && hid->driver &&
+				     urb->actual_length)) {
+				kdb_put_usb_char(usbhid->inbuf,
+						 interface_to_usbdev(usbhid->intf));
+				break;
+			} else {
+				hid_input_report(urb->context, HID_INPUT_REPORT,
+						 urb->transfer_buffer,
+						 urb->actual_length, 1);
+			}
+
 			/*
 			 * autosuspend refused while keys are pressed
 			 * because most keyboards don't wake up when
@@ -1150,6 +1160,7 @@ static int usbhid_start(struct hid_device *hid)
 				USB_INTERFACE_PROTOCOL_KEYBOARD) {
 		usbhid_set_leds(hid);
 		device_set_wakeup_enable(&dev->dev, 1);
+		kdb_keyboard_attach(dev);
 	}
 	return 0;
 
@@ -1174,6 +1185,7 @@ static void usbhid_stop(struct hid_device *hid)
 	if (hid->quirks & HID_QUIRK_ALWAYS_POLL)
 		usbhid->intf->needs_remote_wakeup = 0;
 
+	kdb_keyboard_detach(hid_to_usb_dev(hid));
 	clear_bit(HID_STARTED, &usbhid->iofl);
 	spin_lock_irq(&usbhid->lock);	/* Sync with error and led handlers */
 	set_bit(HID_DISCONNECTED, &usbhid->iofl);
