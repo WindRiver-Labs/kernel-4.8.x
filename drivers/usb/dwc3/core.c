16d8141f462f56160b356e080f6e004287ece3d9
Xulin Sun <xulin.sun@windriver.com>
2016-10-09 16:02:40 +0800

    usb: dwc3: core: add the missing unified device interface replacement
    Reference the commit 3d128919b7e5 (usb: dwc3: core: convert to unified device
    property interface), use the unified device interface to replace the
    "of_property*" APIs, here add the missed ones and to fix below build error:
    
    drivers/usb/dwc3/core.c:989:6: error: 'node' undeclared (first use in this function)
    |   if (node && of_dma_is_coherent(node)) {
    |       ^~~~
    
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -985,7 +985,7 @@ static int dwc3_probe(struct platform_device *pdev)
 		dwc3_readl(dwc->regs, DWC3_GSBUSCFG1) | 0xf00);
 
 	/* Enable Snooping */
-	if (node && of_dma_is_coherent(node)) {
+	if (device_property_read_bool(dev, "dma-coherent")) {
 		dwc3_writel(dwc->regs, DWC3_GSBUSCFG0,
 				dwc3_readl(dwc->regs, DWC3_GSBUSCFG0) | 0x22220000);
 		dev_dbg(dev, "enabled snooping for usb\n");
