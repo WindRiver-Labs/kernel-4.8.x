5cec6e1f79152a830ba27d4f873f58fd354286d4
Ville Syrjälä <ville.syrjala@linux.intel.com>
2016-12-05 16:13:28 +0200

    drm/i915: Protect DSPARB registers with a spinlock
    commit 467a14d935c6d95b94e4197bf63e264eab4c5d06 upstream
    
    Each DSPARB register can house bits for two separate pipes, hence
    we must protect the registers during reprogramming so that parallel
    FIFO reconfigurations happening simultaneosly on multiple pipes won't
    corrupt each others values.
    
    We'll use a new spinlock for this instead of the wm_mutex since we'll
    have to move the DSPARB programming to happen from the vblank evade
    critical section, and we can't use mutexes in there.
    
    v2: Document why we use a spinlock instead of a mutex (Maarten)
    
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1480947208-18468-1-git-send-email-ville.syrjala@linux.intel.com
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -1234,6 +1234,8 @@ static void vlv_pipe_set_fifo_size(struct intel_crtc *crtc)
 		      pipe_name(crtc->pipe), sprite0_start,
 		      sprite1_start, fifo_size);
 
+	spin_lock(&dev_priv->wm.dsparb_lock);
+
 	switch (crtc->pipe) {
 		uint32_t dsparb, dsparb2, dsparb3;
 	case PIPE_A:
@@ -1290,6 +1292,10 @@ static void vlv_pipe_set_fifo_size(struct intel_crtc *crtc)
 	default:
 		break;
 	}
+
+	POSTING_READ(DSPARB);
+
+	spin_unlock(&dev_priv->wm.dsparb_lock);
 }
 
 #undef VLV_FIFO
