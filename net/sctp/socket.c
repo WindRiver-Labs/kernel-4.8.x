e8be1c72aefcb4a701d76f7e47e81a942d803e6c
Xin Long <lucien.xin@gmail.com>
2016-12-15 23:05:52 +0800

    sctp: sctp_transport_lookup_process should rcu_read_unlock when transport is null
    commit 08abb79542c9e8c367d1d8e44fe1026868d3f0a7 upstream.
    
    Prior to this patch, sctp_transport_lookup_process didn't rcu_read_unlock
    when it failed to find a transport by sctp_addrs_lookup_transport.
    
    This patch is to fix it by moving up rcu_read_unlock right before checking
    transport and also to remove the out path.
    
    Fixes: 1cceda784980 ("sctp: fix the issue sctp_diag uses lock_sock in rcu_read_lock")
    Signed-off-by: Xin Long <lucien.xin@gmail.com>
    Acked-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- net/sctp/socket.c
+++ net/sctp/socket.c
@@ -4471,21 +4471,20 @@ int sctp_transport_lookup_process(int (*cb)(struct sctp_transport *, void *),
 				  const union sctp_addr *paddr, void *p)
 {
 	struct sctp_transport *transport;
-	int err = -ENOENT;
+	int err;
 
 	rcu_read_lock();
 	transport = sctp_addrs_lookup_transport(net, laddr, paddr);
+	rcu_read_unlock();
 	if (!transport || !sctp_transport_hold(transport))
-		goto out;
+		return -ENOENT;
 
 	sctp_association_hold(transport->asoc);
 	sctp_transport_put(transport);
 
-	rcu_read_unlock();
 	err = cb(transport, p);
 	sctp_association_put(transport->asoc);
 
-out:
 	return err;
 }
 EXPORT_SYMBOL_GPL(sctp_transport_lookup_process);
