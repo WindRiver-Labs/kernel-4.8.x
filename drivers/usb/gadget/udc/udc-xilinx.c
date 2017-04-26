7db79e81d29c20c53f8c651d55b5b473f5b58c33
Manish Narani <manish.narani@xilinx.com>
2017-04-26 13:38:06 +0530

    usb: gadget: udc-xilinx: Resolve kernel-doc warnings
    commit 6c6a47a8c8e60c5063c04c7d377aa048d36564b6 from
    https://github.com/Xilinx/linux-xlnx.git
    
    This patch resolves kernel-doc warnings for Xilinx UDC driver.
    
    Signed-off-by: Manish Narani <mnarani@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- drivers/usb/gadget/udc/udc-xilinx.c
+++ drivers/usb/gadget/udc/udc-xilinx.c
@@ -1408,7 +1408,6 @@ err:
 /**
  * xudc_stop - stops the device.
  * @gadget: pointer to the usb gadget structure
- * @driver: pointer to usb gadget driver structure
  *
  * Return: zero always
  */
