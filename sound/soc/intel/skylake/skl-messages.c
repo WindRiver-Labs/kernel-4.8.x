5e1570cb2dae6b9e178fee617e44fc4a21ba4138
Anamika Lal <anamikax.lal@intel.com>
2017-05-03 01:20:41 +0530

    ASoC: Intel: Skylake: Add user notification event for pipe creation/deletion
    commit ef65d0c94c69e8401f434a6a0c3a1b93e1f912e6 from
    git://git@github.com:01org/linux-intel-4.9.git
    
    This patch provides kernel event generation on pipeline creation/deletion
    and also update the debugfs file with event timestamp.
    Userspace gets the event for each pipe creation and deletion and can read
    the event timestamp from the debugfs.
    
    Change-Id: I6e015e5f3e5285ecad215a37dfe286a4e3dc3435
    Signed-off-by: Guneshwor Singh <guneshwor.o.singh@intel.com>
    Signed-off-by: Anamika Lal <anamikax.lal@intel.com>
    Reviewed-on: https://git-gar-1.devtools.intel.com/gerrit/15149
    Reviewed-by: audio_build <audio_build@intel.com>
    Reviewed-by: Prusty, Subhransu S <subhransu.s.prusty@intel.com>
    Reviewed-by: Koul, Vinod <vinod.koul@intel.com>
    Tested-by: Sm, Bhadur A <bhadur.a.sm@intel.com>

--- sound/soc/intel/skylake/skl-messages.c
+++ sound/soc/intel/skylake/skl-messages.c
@@ -2206,6 +2206,7 @@ int skl_create_pipeline(struct skl_sst *ctx, struct skl_pipe *pipe)
 	}
 
 	pipe->state = SKL_PIPE_CREATED;
+	skl_dbg_event(ctx, pipe->state);
 
 	return 0;
 }
@@ -2244,6 +2245,7 @@ int skl_delete_pipe(struct skl_sst *ctx, struct skl_pipe *pipe)
 	}
 
 	pipe->state = SKL_PIPE_INVALID;
+	skl_dbg_event(ctx, pipe->state);
 
 	return ret;
 }
