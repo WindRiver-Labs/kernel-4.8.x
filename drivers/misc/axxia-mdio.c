a8d480416977acba904b2eafb707a7e09df5a757
John Jacques <john.jacques@intel.com>
2017-05-15 18:34:03 +0300

    axxia: Clean Up the MDIO Access Code
    Removes unused code and adds the second MDIO bus
    available on 6700.
    
    Signed-off-by: John Jacques <john.jacques@intel.com>

--- drivers/misc/axxia-mdio.c
+++ drivers/misc/axxia-mdio.c
@@ -1,12 +1,18 @@
 /*
- * Driver for MDIO controller found in LSI Axxia devices.
- *
- * Copyright (C) 2013 LSI Corporation
+ * Copyright (C) 2017 Intel <john.jacques@intel.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.
  */
 
 #include <linux/module.h>
@@ -37,7 +43,7 @@
 #define MDIO_CLK_PERIOD		0x0c
 
 /* MDIO bus driver private data */
-struct lsi_mdio_priv {
+struct axxia_mdio_priv {
 	void __iomem *base;
 	struct mii_bus *bus;
 };
@@ -45,11 +51,11 @@ struct lsi_mdio_priv {
 static inline void __iomem *
 bus_to_regs(struct mii_bus *bus)
 {
-	return ((struct lsi_mdio_priv *)bus->priv)->base;
+	return ((struct axxia_mdio_priv *)bus->priv)->base;
 }
 
 static int
-lsi_mdio_read(struct mii_bus *bus, int mii_id, int regnum)
+axxia_mdio_read(struct mii_bus *bus, int mii_id, int regnum)
 {
 	void __iomem *base = bus_to_regs(bus);
 	u32 ctrl;
@@ -62,8 +68,10 @@ lsi_mdio_read(struct mii_bus *bus, int mii_id, int regnum)
 	ctrl = (CONTROL_READ |
 		CONTROL_PHYID(mii_id) |
 		CONTROL_PHYREG(regnum));
+
 	if (regnum & MII_ADDR_C45)
 		ctrl |= CONTROL_CL45;
+
 	writel(ctrl, base + MDIO_CONTROL);
 
 	/* Wait for the mdio_done (status) bit to clear. */
@@ -79,10 +87,12 @@ lsi_mdio_read(struct mii_bus *bus, int mii_id, int regnum)
 }
 
 static int
-lsi_mdio_write(struct mii_bus *bus, int mii_id, int regnum, u16 value)
+axxia_mdio_write(struct mii_bus *bus, int mii_id, int regnum, u16 value)
 {
 	void __iomem *base = bus_to_regs(bus);
 	u32 ctrl;
+
+	printk("%s:%d - base=0x%p\n", __FILE__, __LINE__, base);
 
 	/* Wait for mdio_busy (control) to be clear. */
 	while ((readl(base + MDIO_CONTROL) & CONTROL_BUSY) != 0)
@@ -96,8 +106,10 @@ lsi_mdio_write(struct mii_bus *bus, int mii_id, int regnum, u16 value)
 		CONTROL_PHYID(mii_id) |
 		CONTROL_PHYREG(regnum) |
 		CONTROL_DATA(value));
+
 	if (regnum & MII_ADDR_C45)
 		ctrl |= CONTROL_CL45;
+
 	writel(ctrl, base + MDIO_CONTROL);
 
 	/* Wait for the mdio_done (status) bit to clear. */
@@ -112,34 +124,39 @@ lsi_mdio_write(struct mii_bus *bus, int mii_id, int regnum, u16 value)
 }
 
 static int
-lsi_mdio_probe(struct platform_device *pdev)
+axxia_mdio_probe(struct platform_device *pdev)
 {
-	struct device_node   *np = pdev->dev.of_node;
-	struct lsi_mdio_priv *priv = NULL;
-	struct resource      *res;
-	int                   err;
-	u32                   clk_offset = 0x10;
-	u32                   clk_period = 0x2c;
+	struct device_node     *np = pdev->dev.of_node;
+	struct axxia_mdio_priv *priv = NULL;
+	struct resource        *res;
+	int                     err;
+	u32                     clk_offset = 0x10;
+	u32                     clk_period = 0x2c;
 
 	priv = devm_kzalloc(&pdev->dev, sizeof(*priv), GFP_KERNEL);
+
 	if (!priv)
 		return -ENOMEM;
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+
 	if (!res)
 		return -ENODEV;
 
 	priv->bus = mdiobus_alloc();
+
 	if (!priv->bus)
 		return -ENOMEM;
 
 	priv->bus->name  = "Axxia MDIO",
-	priv->bus->read  = lsi_mdio_read,
-	priv->bus->write = lsi_mdio_write,
+	priv->bus->read  = axxia_mdio_read,
+	priv->bus->write = axxia_mdio_write,
 	priv->bus->priv  = priv;
 	snprintf(priv->bus->id, MII_BUS_ID_SIZE, pdev->name);
+	printk("%s:%d - pdev->name=%s\n", __FILE__, __LINE__, pdev->name);
 
 	priv->base = devm_ioremap_resource(&pdev->dev, res);
+
 	if (!priv->base) {
 		dev_err(&pdev->dev, "Failed to map registers\n");
 		err = -ENODEV;
@@ -157,6 +174,7 @@ lsi_mdio_probe(struct platform_device *pdev)
 	writel(0, priv->base + MDIO_CONTROL);
 
 	err = of_mdiobus_register(priv->bus, np);
+
 	if (err) {
 		dev_err(&pdev->dev, "Failed to register MDIO bus\n");
 		goto err_ret;
@@ -165,13 +183,15 @@ lsi_mdio_probe(struct platform_device *pdev)
 	return 0;
 
 err_ret:
+
 	if (priv && priv->bus)
 		kfree(priv->bus);
+
 	return err;
 }
 
 static int
-lsi_mdio_remove(struct platform_device *pdev)
+axxia_mdio_remove(struct platform_device *pdev)
 {
 	struct mii_bus *bus = dev_get_drvdata(&pdev->dev);
 
@@ -182,24 +202,27 @@ lsi_mdio_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static struct of_device_id lsi_mdio_match[] = {
+static struct of_device_id axxia_mdio_match[] = {
 	{ .compatible = "lsi,axm-mdio", },
+	{ .compatible = "intel,axxia-mdio0", },
+	{ .compatible = "intel,axxia-mdio1", },
 	{},
 };
-MODULE_DEVICE_TABLE(of, lsi_mdio_match);
 
-static struct platform_driver lsi_mdio_driver = {
+MODULE_DEVICE_TABLE(of, axxia_mdio_match);
+
+static struct platform_driver axxia_mdio_driver = {
 	.driver = {
-		.name           = "lsi-mdio",
+		.name           = "axxia-mdio",
 		.owner          = THIS_MODULE,
-		.of_match_table = lsi_mdio_match,
+		.of_match_table = axxia_mdio_match,
 	},
-	.probe  = lsi_mdio_probe,
-	.remove = lsi_mdio_remove,
+	.probe  = axxia_mdio_probe,
+	.remove = axxia_mdio_remove,
 };
 
-module_platform_driver(lsi_mdio_driver);
+module_platform_driver(axxia_mdio_driver);
 
-MODULE_AUTHOR("LSI Corporation");
-MODULE_DESCRIPTION("LSI MDIO Bus Driver");
+MODULE_AUTHOR("John Jacques");
+MODULE_DESCRIPTION("Axxia MDIO Bus Driver");
 MODULE_LICENSE("GPL v2");
---
+++ include/linux/axxia-mdio.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2016 Intel <john.jacques@intel.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.
+ */
+
+#ifndef __DRIVERS_MISC_AXXIA_MDIO_H
+#define __DRIVERS_MISC_AXXIA_MDIO_H
+
+#endif /* __DRIVERS_MISC_AXXIA_MDIO_H */
