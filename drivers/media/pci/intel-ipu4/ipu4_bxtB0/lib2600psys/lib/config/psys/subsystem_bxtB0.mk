eb08e0ec533e8cb50420ccbf9c603b1bbaa8503e
Ryan Metcalfe <ryan.d.metcalfe@intel.com>
2017-06-12 11:53:32 -0400

    Initial overlay of intel-ipu4 MR2 driver (no changes yet, build fails)
    [Kevin: The original patch is taken from a tarball from Ryan, drop the
    config_APL_IPU4 and fix a build error due to the get_user_pages() API
    change]
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>
    
    squash! Initial overlay of intel-ipu4 MR2 driver (no changes yet, build fails)

---
+++ drivers/media/pci/intel-ipu4/ipu4_bxtB0/lib2600psys/lib/config/psys/subsystem_bxtB0.mk
@@ -0,0 +1,31 @@
+# #
+# Support for Intel Camera Imaging ISP subsystem.
+# Copyright (c) 2010 - 2016, Intel Corporation.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details
+#
+
+############################################################################
+# This file is used to specify versions and properties of PSYS firmware
+# components. Please note that these are subsystem specific. System specific
+# properties should go to system_$IPU_SYSVER.mk. Also the device versions
+# should be defined under "devices" or should be taken from the SDK.
+############################################################################
+
+# Activate loading params and storing stats DDR<->REGs with DMA
+PSYS_USE_ISA_DMA                 = 1
+
+# Used in ISA module
+PSYS_ISL_DPC_DPC_V2              = 0
+
+# Assume OFS will be running concurrently with IPF, and prioritize according to rates of services on devproxy
+CONCURRENT_OFS_IPF_PRIORITY_OPTIMIZATION_ENABLED	= 1
+
+HAS_GMEM						= 1
