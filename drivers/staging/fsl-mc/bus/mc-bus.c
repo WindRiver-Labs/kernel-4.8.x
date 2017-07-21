2e86d2a79240e63ac43476b86ab8db1c7c14b988
Limeng <Meng.Li@windriver.com>
2017-07-21 18:02:11 +0800

    mc-bus: Do not destroy DPMCP MC I/O object on device remove
     We do not create MC I/O object in device add, so we should not
     destroy the MC I/O object on device remove. MC I/O object for
     DPMCP is created in fsl_mc_portal_allocate() and freed in
     fsl_mc_portal_free().
    
     Signed-off-by: Bharat Bhushan <Bharat.Bhushan@freescale.com>
     [Original patch taken from QorIQ-SDK-V2.0-20160527-yocto]
     Signed-off-by: Yanjiang Jin <yanjiang.jin@windriver.com>

--- drivers/staging/fsl-mc/bus/mc-bus.c
+++ drivers/staging/fsl-mc/bus/mc-bus.c
@@ -666,6 +666,8 @@ void fsl_mc_device_remove(struct fsl_mc_device *mc_dev)
 		if (&mc_dev->dev == fsl_mc_bus_type.dev_root)
 			fsl_mc_bus_type.dev_root = NULL;
 	}
+	else
+		WARN_ON(mc_dev->mc_io != NULL);
 
 	kfree(mc_dev->driver_override);
 	mc_dev->driver_override = NULL;
