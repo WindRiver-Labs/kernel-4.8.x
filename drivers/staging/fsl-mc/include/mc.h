ab6908a0eb65d93784245c9740b5be1009a6e3ec
Limeng <Meng.Li@windriver.com>
2017-07-24 16:10:27 +0800

    staging: fsl-mc: Allow the MC bus driver to run without GIC support
    If the lpi-parent property is not present in the fsl,qoriq-mc node
    of the device tree, the MC bus driver will assume that the GIC is not
    supported.
    
    This change is made in order to be able to use the MC bus driver in a
    KVM VM, without having GIC-ITS support in guests. Added function
    fsl_mc_interrupts_supported(), which can be called from DPAA2 object
    drivers.
    
    Signed-off-by: J. German Rivera <German.Rivera@freescale.com>
    Change-Id: I881ab2c45c949e55cfafe1d281a7a31560955e5b
    Reviewed-on: http://git.am.freescale.net:8181/34712
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Stuart Yoder <stuart.yoder@freescale.com>
    [Original patch taken from QorIQ-SDK-V2.0-20160527-yocto]
    Signed-off-by: Yanjiang Jin <yanjiang.jin@windriver.com>

--- drivers/staging/fsl-mc/include/mc.h
+++ drivers/staging/fsl-mc/include/mc.h
@@ -189,6 +189,8 @@ int __must_check __fsl_mc_driver_register(struct fsl_mc_driver *fsl_mc_driver,
 
 void fsl_mc_driver_unregister(struct fsl_mc_driver *driver);
 
+bool fsl_mc_interrupts_supported(void);
+
 bool fsl_mc_bus_exists(void);
 
 int __must_check fsl_mc_portal_allocate(struct fsl_mc_device *mc_dev,
