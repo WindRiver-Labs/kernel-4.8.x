5530fdaeea5daaf1f2604128b2bf4157eba81cf0
Zumeng Chen <zumeng.chen@windriver.com>
2017-09-25 13:35:54 +0800

    ARM: OMAP2+: Use pdata-quirks for sgx deassert_hardreset
    Use pdata_quirks to provide platform data to the sgx driver.  This is
    used to provide a function pointer for the sgx driver to access
    omap_device_deassert_hardreset along with the reset name as defined in
    the corresponding hwmod entry.
    
    This platform data will not be required when a separate reset driver is
    available allowing decoupling from omap_hwmod and omap_device.
    
    Signed-off-by: Darren Etheridge <detheridge@ti.com>
    Signed-off-by: Eric Ruei <e-ruei1@ti.com>

--- arch/arm/mach-omap2/pdata-quirks.c
+++ arch/arm/mach-omap2/pdata-quirks.c
@@ -29,6 +29,7 @@
 #include <linux/platform_data/pwm_omap_dmtimer.h>
 #include <linux/platform_data/media/ir-rx51.h>
 #include <linux/platform_data/asoc-ti-mcbsp.h>
+#include <linux/platform_data/sgx-omap.h>
 #include <plat/dmtimer.h>
 
 #include "common.h"
@@ -60,6 +61,14 @@ static inline int omap_iommu_set_pwrdm_constraint(struct platform_device *pdev,
 {
 	return 0;
 }
+#endif
+
+#if defined(CONFIG_SOC_AM33XX) || defined(CONFIG_SOC_AM43XX)
+static struct gfx_sgx_platform_data sgx_pdata = {
+	.reset_name = "gfx",
+	.assert_reset = omap_device_assert_hardreset,
+	.deassert_reset = omap_device_deassert_hardreset,
+};
 #endif
 
 #ifdef CONFIG_MACH_NOKIA_N8X0
@@ -594,6 +603,8 @@ static struct of_dev_auxdata omap_auxdata_lookup[] __initdata = {
 		       &wkup_m3_data),
 	OF_DEV_AUXDATA("ti,am3356-pruss-soc-bus", 0x4a326000,
 		       "4a326000.pruss-soc-bus", &pruss_pdata),
+	OF_DEV_AUXDATA("ti,am3352-sgx530", 0x56000000, "56000000.sgx",
+		       &sgx_pdata),
 #endif
 #ifdef CONFIG_SOC_AM43XX
 	OF_DEV_AUXDATA("ti,am4372-wkup-m3", 0x44d00000, "44d00000.wkup_m3",
