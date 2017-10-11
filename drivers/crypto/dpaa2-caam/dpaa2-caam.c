7163aab934b80550f5a57104c3542be878697eef
Limeng <Meng.Li@windriver.com>
2017-10-11 13:34:00 +0800

    Revert "crypto: dpaa2-caam: uprev to DPSECI vers 5.0"
    This reverts commit 8e24a8468fd309b5ab5f45b84ba61285bae32523.
    
    At first, dpaa2-caam patches are from nxp-sdk-2.0 that is based on
    kernel4.1. But in kernel4.8, there is a obvious changing on crypto
    software architecture, so modify code to pass compilation, but this
    feature is not verified.
    Now, there is an open source sdk that is based on kernel4.4. In
    kernel4.4, crypto software architecture is almost the same with
    kernel4.8, so we get dpaa2-caam patches from open source sdk, and
    revert patches from nxp-sdk-2.0.
    
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/crypto/dpaa2-caam/dpaa2-caam.c
+++ drivers/crypto/dpaa2-caam/dpaa2-caam.c
@@ -5037,18 +5037,11 @@ static int __cold dpaa2_dpseci_setup(struct fsl_mc_device *ls_dev)
 
 	dev_info(dev, "Opened dpseci object successfully\n");
 
-	err = dpseci_get_api_version(priv->mc_io, 0, &priv->major_ver,
-					&priv->minor_ver);
-	if (err) {
-		dev_err(dev, "dpseci_get_api_version() failed\n");
-		goto err_get_vers;
-	}
-
 	err = dpseci_get_attributes(priv->mc_io, 0, ls_dev->mc_handle,
 				    &priv->dpseci_attr);
 	if (err) {
 		dev_err(dev, "dpseci_get_attributes() failed\n");
-		goto err_get_vers;
+		goto err_get_attr;
 	}
 
 	priv->num_pairs = min(priv->dpseci_attr.num_rx_queues,
@@ -5059,7 +5052,7 @@ static int __cold dpaa2_dpseci_setup(struct fsl_mc_device *ls_dev)
 					  &priv->rx_queue_attr[i]);
 		if (err) {
 			dev_err(dev, "dpseci_get_rx_queue() failed\n");
-			goto err_get_vers;
+			goto err_get_attr;
 		}
 	}
 
@@ -5068,7 +5061,7 @@ static int __cold dpaa2_dpseci_setup(struct fsl_mc_device *ls_dev)
 					  &priv->tx_queue_attr[i]);
 		if (err) {
 			dev_err(dev, "dpseci_get_tx_queue() failed\n");
-			goto err_get_vers;
+			goto err_get_attr;
 		}
 	}
 
@@ -5091,7 +5084,7 @@ static int __cold dpaa2_dpseci_setup(struct fsl_mc_device *ls_dev)
 
 	return 0;
 
-err_get_vers:
+err_get_attr:
 	dpseci_close(priv->mc_io, 0, ls_dev->mc_handle);
 err_open:
 	return err;
@@ -5116,8 +5109,8 @@ static int dpaa2_dpseci_enable(struct dpaa2_caam_priv *priv)
 	}
 
 	dev_info(dev, "DPSECI version %d.%d\n",
-		 priv->major_ver,
-		 priv->minor_ver);
+		 priv->dpseci_attr.version.major,
+		 priv->dpseci_attr.version.minor);
 
 	return 0;
 }
