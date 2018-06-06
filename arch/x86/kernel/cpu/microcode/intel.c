0e21631a7e16fc611dd18af4854b4d174fcc92a6
Yue Tao <Yue.Tao@windriver.com>
2018-06-06 15:00:15 +0800

    Revert "x86/microcode/intel: Save microcode patch unconditionally"
    This reverts commit 66f625e56b8c6475f5a4305f0637d9187bfea09f.

--- arch/x86/kernel/cpu/microcode/intel.c
+++ arch/x86/kernel/cpu/microcode/intel.c
@@ -313,6 +313,7 @@ static void show_saved_mc(void)
  */
 static void save_mc_for_early(u8 *mc, unsigned int size)
 {
+#ifdef CONFIG_HOTPLUG_CPU
 	/* Synchronization during CPU hotplug. */
 	static DEFINE_MUTEX(x86_cpu_microcode_mutex);
 
@@ -322,6 +323,7 @@ static void save_mc_for_early(u8 *mc, unsigned int size)
 	show_saved_mc();
 
 	mutex_unlock(&x86_cpu_microcode_mutex);
+#endif
 }
 
 static bool load_builtin_intel_microcode(struct cpio_data *cp)
